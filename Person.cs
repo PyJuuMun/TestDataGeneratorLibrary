63df9ce9f6ed2b710e845176ef255a929229bd95
PyJuuMun <pyjuumun@hotmail.com>
2020-04-09 13:56:36 +0300

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace TestDataGeneratorLibrary
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public int Age;
+
+        public Person()
+        {
+            
+        }
+
+        public static string[] FirstNameMales()
+        {
+            string[] firstname = { "Jaakko", "Tommi", "Juho", "Matias", "Niko", "Pertti", "Markus", "Eetu" };
+            return firstname;
+        }
+    }
+}
